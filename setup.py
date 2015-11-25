948ff6bcdfd38de78c8e96168bc656efa741fdd6
Russell Avdek <russell@lovely.com>
2015-11-25 08:52:02 -0800

    Undoing README ext changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 install_requires = ['docopt', 'beautifulsoup4']
 
 readme = ""
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
